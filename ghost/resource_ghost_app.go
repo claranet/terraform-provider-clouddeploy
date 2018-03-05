6910bed8e4345fc347e986f063d155171679651d
Jordan Caussat <jordan.caussat@arrayent.com>
2018-03-05 14:04:43 +0100

    TER-233: Update app resource: add new variables to parse

--- ghost/resource_ghost_app.go
+++ ghost/resource_ghost_app.go
@@ -27,6 +27,312 @@ func resourceGhostApp() *schema.Resource {
 				Type:     schema.TypeString,
 				Required: true,
 			},
+
+			"region": {
+				Type:     schema.TypeString,
+				Required: true,
+			},
+			"vpc_id": {
+				Type:     schema.TypeString,
+				Required: true,
+			},
+			"instance_type": {
+				Type:     schema.TypeString,
+				Required: true,
+			},
+			"detailled_monitoring": {
+				Type:     schema.TypeBool,
+				Optional: true,
+				Default:  0,
+			},
+
+			"autoscale": {
+				Type:     schema.TypeList,
+				Required: true,
+				MaxItems: 1,
+				Elem: &schema.Resource{
+					Schema: map[string]*schema.Schema{
+						"name": {
+							Type:     schema.TypeString,
+							Required: true,
+						},
+						"metrics": {
+							Type:     schema.TypeBool,
+							Optional: true,
+							Default:  false,
+						},
+						"min": {
+							Type:     schema.TypeInt,
+							Optional: true,
+							Default:  1,
+						},
+						"max": {
+							Type:     schema.TypeInt,
+							Optional: true,
+							Default:  3,
+						},
+					},
+				},
+			},
+
+			"load_balancer": {
+				Type:     schema.TypeList,
+				Optional: true,
+				MaxItems: 1,
+				Elem: &schema.Resource{
+					Schema: map[string]*schema.Schema{
+						"type": {
+							Type:     schema.TypeString,
+							Required: true,
+							Default:  "elb",
+						},
+						"wait_before_deploy": {
+							Type:     schema.TypeInt,
+							Optional: true,
+							Default:  10,
+						},
+						"wait_after_deploy": {
+							Type:     schema.TypeInt,
+							Optional: true,
+							Default:  10,
+						},
+					},
+				},
+			},
+
+			"build_infos": {
+				Type:     schema.TypeList,
+				Required: true,
+				MaxItems: 1,
+				Elem: &schema.Resource{
+					Schema: map[string]*schema.Schema{
+						"ssh_username": {
+							Type:    schema.TypeString,
+							Default: "admin",
+						},
+						"source_ami": {
+							Type:     schema.TypeString,
+							Required: true,
+						},
+						"subnet_id": {
+							Type:     schema.TypeString,
+							Required: true,
+						},
+					},
+				},
+			},
+
+			"environment_infos": {
+				Type:     schema.TypeList,
+				Required: true,
+				MaxItems: 1,
+				Elem: &schema.Resource{
+					Schema: map[string]*schema.Schema{
+						"instance_profile": {
+							Type:     schema.TypeString,
+							Required: true,
+						},
+						"key_name": {
+							Type:     schema.TypeString,
+							Required: true,
+						},
+						"public_ip": {
+							Type:     schema.TypeBool,
+							Required: true,
+						},
+						"root_block_device": {
+							Type:     schema.TypeMap,
+							Optional: true,
+							Elem: &schema.Resource{
+								Schema: map[string]*schema.Schema{
+									"size": {
+										Type:     schema.TypeInt,
+										Optional: true,
+										Default:  20,
+									},
+									"name": {
+										Type:     schema.TypeString,
+										Optional: true,
+									},
+								},
+							},
+						},
+						"security_groups": {
+							Type:     schema.TypeSet,
+							Elem:     &schema.Schema{Type: schema.TypeString},
+							Optional: true,
+							Set:      schema.HashString,
+						},
+						"instance_tags": {
+							Type:     schema.TypeMap,
+							Optional: true,
+							Elem: &schema.Resource{
+								Schema: map[string]*schema.Schema{
+									"name": {
+										Type:     schema.TypeString,
+										Required: true,
+									},
+									"value": {
+										Type:     schema.TypeString,
+										Required: true,
+									},
+								},
+							},
+						},
+						"subnet_ids": {
+							Type:     schema.TypeSet,
+							Elem:     &schema.Schema{Type: schema.TypeString},
+							Computed: true,
+							Set:      schema.HashString,
+						},
+						"optional_volumes": {
+							Type:     schema.TypeList,
+							Optional: true,
+							Elem: &schema.Resource{
+								Schema: map[string]*schema.Schema{
+									"name": {
+										Type:     schema.TypeString,
+										Optional: true,
+									},
+									"type": {
+										Type:     schema.TypeString,
+										Optional: true,
+										Default:  "gp2",
+									},
+									"size": {
+										Type:     schema.TypeInt,
+										Optional: true,
+										Default:  20,
+									},
+									"iops": {
+										Type:     schema.TypeInt,
+										Optional: true,
+									},
+									"attach_volume_during_buildimage": {
+										Type:     schema.TypeBool,
+										Optional: true,
+									},
+								},
+							},
+						},
+					},
+				},
+			},
+
+			"environment_variables": {
+				Type:     schema.TypeList,
+				Optional: true,
+				Elem: &schema.Resource{
+					Schema: map[string]*schema.Schema{
+						"key": {
+							Type:     schema.TypeString,
+							Required: true,
+						},
+						"value": {
+							Type:     schema.TypeString,
+							Required: true,
+						},
+					},
+				},
+			},
+
+			"log_notifications": {
+				Type:     schema.TypeList,
+				Elem:     &schema.Schema{Type: schema.TypeString},
+				Optional: true,
+			},
+
+			"features": {
+				Type:     schema.TypeList,
+				Required: false,
+				Elem: &schema.Resource{
+					Schema: map[string]*schema.Schema{
+						"name": {
+							Type:     schema.TypeString,
+							Required: true,
+						},
+						"version": {
+							Type:     schema.TypeString,
+							Required: true,
+						},
+					},
+				},
+			},
+
+			"lifecycle_hooks": {
+				Type:     schema.TypeList,
+				Required: false,
+				MaxItems: 1,
+				Elem: &schema.Resource{
+					Schema: map[string]*schema.Schema{
+						"pre_build_image": {
+							Type:     schema.TypeString,
+							Optional: true,
+						},
+						"post_build_image": {
+							Type:     schema.TypeString,
+							Optional: true,
+						},
+						"pre_bootstrap": {
+							Type:     schema.TypeString,
+							Optional: true,
+						},
+						"post_boostrap": {
+							Type:     schema.TypeString,
+							Optional: true,
+						},
+					},
+				},
+			},
+
+			"modules": {
+				Type:     schema.TypeList,
+				Required: true,
+				Elem: &schema.Resource{
+					Schema: map[string]*schema.Schema{
+						"name": {
+							Type:     schema.TypeString,
+							Required: true,
+						},
+						"git_repo": {
+							Type:     schema.TypeString,
+							Required: true,
+						},
+						"path": {
+							Type:     schema.TypeString,
+							Required: true,
+						},
+						"UID": {
+							Type:     schema.TypeString,
+							Optional: true,
+						},
+						"GID": {
+							Type:     schema.TypeString,
+							Optional: true,
+						},
+						"scope": {
+							Type:     schema.TypeString,
+							Required: true,
+						},
+						"build_pack": {
+							Type:     schema.TypeString,
+							Optional: true,
+						},
+						"pre_deploy": {
+							Type:     schema.TypeString,
+							Optional: true,
+						},
+						"post_deploy": {
+							Type:     schema.TypeString,
+							Optional: true,
+						},
+						"after_all_deploy": {
+							Type:     schema.TypeString,
+							Optional: true,
+						},
+					},
+				},
+			},
 		},
 	}
 }
