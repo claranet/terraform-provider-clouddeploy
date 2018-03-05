fb282100d88d22b8f4dc294ada7b4e8aa4bf4728
Jordan Caussat <jordan.caussat@arrayent.com>
2018-03-05 11:15:49 +0100

    TER-233: Update example and add password variable

--- examples/main.tf
+++ examples/main.tf
@@ -1,9 +1,56 @@
 provider "ghost" {
-  user = "test"
-  password = "test"
-  endpoint = "localhost"
+  user     = "demo"
+  password = "${var.password}"
+  endpoint = "https://demo.ghost.morea.fr"
 }
 
-resource "ghost_app" "my-app" {
-  name = "test"
+resource "ghost_app" "wordpress" {
+  name = "wordpress"
+  env  = "prod"
+  role = "webfront"
+
+  region        = "eu-west-1"
+  instance_type = "t2.micro"
+  vpc_id        = "vpc-3f1eb65a"
+
+  log_notifications = [
+    "ghost-devops@domain.com",
+  ]
+
+  build_infos = {
+    subnet_id    = "subnet-a7e849fe"
+    ssh_username = "admin"
+    source_ami   = "ami-03ce4474"
+  }
+
+  environment_infos = {
+    instance_profile  = "iam.ec2.demo"
+    key_name          = "ghost-demo"
+    root_block_device = {}
+    optional_volumes  = []
+    subnet_ids        = ["subnet-a7e849fe"]
+    security_groups   = ["sg-6814f60c"]
+  }
+
+  autoscale = {
+    name = ""
+  }
+
+  module = {
+    name       = "symfony2"
+    pre_deploy = "ZXhpdCAx"
+    path       = "/var/www"
+    scope      = "code"
+    git_repo   = "https://github.com/KnpLabs/KnpIpsum.git"
+  }
+
+  feature = {
+    version = "5.4"
+    name    = "php5"
+  }
+
+  feature = {
+    version = "2.2"
+    name    = "apache2"
+  }
 }
