27fb8d0a9f25f021cc9f6e50ea6c57c1b27042e2
Jordan Caussat <jordan.caussat@fr.clara.net>
2018-03-14 12:19:32 +0100

    TER-240: Add tests for update feature

--- ghost/resource_ghost_app_test.go
+++ ghost/resource_ghost_app_test.go
@@ -26,6 +26,16 @@ func TestAccGhostAppBasic(t *testing.T) {
 					testAccCheckGhostAppExists(resourceName),
 					resource.TestCheckResourceAttr(resourceName, "name", envName),
 					resource.TestCheckResourceAttr(resourceName, "env", "dev"),
+					resource.TestCheckResourceAttr(resourceName, "region", "eu-west-1"),
+				),
+			},
+			{
+				Config: testAccGhostAppConfigUpdated(envName),
+				Check: resource.ComposeAggregateTestCheckFunc(
+					testAccCheckGhostAppExists(resourceName),
+					resource.TestCheckResourceAttr(resourceName, "name", envName),
+					resource.TestCheckResourceAttr(resourceName, "env", "dev"),
+					resource.TestCheckResourceAttr(resourceName, "region", "eu-west-2"),
 				),
 			},
 		},
@@ -147,6 +157,89 @@ func testAccGhostAppConfig(name string) string {
 				environment_variables = [{
 					key   = "myvar"
 					value = "myvalue"
+				}]
+			}
+			`, name)
+}
+
+func testAccGhostAppConfigUpdated(name string) string {
+	return fmt.Sprintf(`
+			resource "ghost_app" "test" {
+				name = "%s"
+			  env  = "dev"
+			  role = "webfront"
+
+			  region        = "eu-west-2"
+			  instance_type = "t2.micro"
+			  vpc_id        = "vpc-3f1eb65a"
+
+			  log_notifications = [
+			    "ghost-devops@domain.com",
+			  ]
+
+			  build_infos = {
+			    subnet_id    = "subnet-a7e849fe"
+			    ssh_username = "admin"
+			    source_ami   = "ami-03ce4474"
+			  }
+
+			  environment_infos = {
+			    instance_profile  = "iam.ec2.demo"
+			    key_name          = "ghost-demo"
+			    root_block_device = {
+						name = "testblockdevice"
+						size = 20
+					}
+			    subnet_ids        = ["subnet-a7e849fe"]
+			    security_groups   = ["sg-6814f60c"]
+					instance_tags			= [{
+						tag_name  = "Name"
+						tag_value = "wordpress"
+					},
+					{
+						tag_name  = "Type"
+						tag_value = "front"
+					}]
+			  }
+
+			  autoscale = {
+			    name = "autoscale"
+					min  = 1
+					max  = 2
+			  }
+
+			  modules = [{
+					name       = "wordpress"
+			    pre_deploy = ""
+			    path       = "/var/www"
+			    scope      = "code"
+			    git_repo   = "https://github.com/KnpLabs/KnpIpsum.git"
+			  },
+				{
+					name        = "wordpress2"
+					pre_deploy  = "ZXhpdCAx"
+					post_deploy = "ZXhpdCAx"
+					path        = "/var/www"
+					scope       = "code"
+					git_repo    = "https://github.com/KnpLabs/KnpIpsum.git"
+				}]
+
+			  features = [{
+			    version = "5.4"
+			    name    = "php5"
+			  },
+				{
+			    version = "2.2"
+			    name    = "apache2"
+			  }]
+
+				lifecycle_hooks = {
+					pre_buildimage  = "#!/usr/bin/env bash"
+				}
+
+				environment_variables = [{
+					key   = "myvar2"
+					value = "myvalue2"
 				}]
 			}
 			`, name)
