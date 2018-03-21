9858c2126298c5d69023ff34ec0a2f2b704833d2
Jordan Caussat <jordan.caussat@fr.clara.net>
2018-03-21 13:55:34 +0100

    TER-248: Add ghost app import test

---
+++ ghost/import_ghost_app_test.go
@@ -0,0 +1,30 @@
+package ghost
+
+import (
+	"fmt"
+	"testing"
+
+	"github.com/hashicorp/terraform/helper/acctest"
+	"github.com/hashicorp/terraform/helper/resource"
+)
+
+func testAccGhostAppImportBasic(t *testing.T) {
+	envName := fmt.Sprintf("import_ghost_app_acc_env_basic_%s", acctest.RandString(10))
+
+	resource.Test(t, resource.TestCase{
+		PreCheck:     func() { testAccPreCheck(t) },
+		Providers:    testAccProviders,
+		CheckDestroy: testAccCheckGhostAppDestroy,
+		Steps: []resource.TestStep{
+			resource.TestStep{
+				Config: testAccGhostAppConfig(envName),
+			},
+
+			resource.TestStep{
+				ResourceName:      "ghost_app.test",
+				ImportState:       true,
+				ImportStateVerify: true,
+			},
+		},
+	})
+}
