5e77332392c562ef9e44903f1caed5f290e5a8e9
Jordan Caussat <jordan.caussat@fr.clara.net>
2018-03-22 17:41:02 +0100

    TER-248: Remove check destroy

--- ghost/import_ghost_app_test.go
+++ ghost/import_ghost_app_test.go
@@ -12,9 +12,8 @@ func testAccGhostAppImportBasic(t *testing.T) {
 	envName := fmt.Sprintf("import_ghost_app_acc_env_basic_%s", acctest.RandString(10))
 
 	resource.Test(t, resource.TestCase{
-		PreCheck:     func() { testAccPreCheck(t) },
-		Providers:    testAccProviders,
-		CheckDestroy: testAccCheckGhostAppDestroy,
+		PreCheck:  func() { testAccPreCheck(t) },
+		Providers: testAccProviders,
 		Steps: []resource.TestStep{
 			resource.TestStep{
 				Config: testAccGhostAppConfig(envName),
