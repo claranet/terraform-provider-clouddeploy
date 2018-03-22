b6c64b1be8dde7b23df158c10c94e2d733bb6092
Jordan Caussat <jordan.caussat@fr.clara.net>
2018-03-22 17:49:02 +0100

    TER-248: activate import test

--- ghost/import_ghost_app_test.go
+++ ghost/import_ghost_app_test.go
@@ -8,7 +8,7 @@ import (
 	"github.com/hashicorp/terraform/helper/resource"
 )
 
-func testAccGhostAppImportBasic(t *testing.T) {
+func TestAccGhostAppImportBasic(t *testing.T) {
 	envName := fmt.Sprintf("import_ghost_app_acc_env_basic_%s", acctest.RandString(10))
 
 	resource.Test(t, resource.TestCase{
