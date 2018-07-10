dc682dde9bd67f62005b48c40a4b881b06bb9d42
Jordan Caussat <jordan.caussat@fr.clara.net>
2018-07-10 15:50:31 +0200

    TER-243: Use gorelease docker image for release instead of binary

--- scripts/bitbucket-pipelines-release.sh
+++ scripts/bitbucket-pipelines-release.sh
@@ -5,4 +5,4 @@ mkdir -pv "${PACKAGE_PATH}"
 tar -cO --exclude-vcs --exclude=bitbucket-pipelines.yml . | tar -xv -C "${PACKAGE_PATH}"
 cd "${PACKAGE_PATH}"
 git ls-remote --heads --tags https://github.com/claranet/terraform-provider-cloud-deploy.git | grep -E "refs/(heads|tags)/${BITBUCKET_TAG}$"
-curl -sL https://git.io/goreleaser | bash
+docker run --rm goreleaser/goreleaser:v0.80
